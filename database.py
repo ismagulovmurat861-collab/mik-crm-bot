fa89e5a1907fe81fd81d35936465e2ba02b635c7
ismagulovmurat861-collab <ismagulovmurat861@gmail.com>
2026-05-13 12:43:08 +0600

    Update database.py

--- database.py
+++ database.py
@@ -3,8 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL=postgres:DZqWAvUhwrHBWabrlJgNBrYbDVmufWnu@yamabiko.proxy.rlwy.net:36404/railway", "sqlite:///./test.db")
+DATABASE_URL = os.getenv("DATABASE_URL=postgres://DZqWAvUhwrHBWabrlJgNBrYbDVmufWnu@yamabiko.proxy.rlwy.net:36404/railway", "sqlite:///./test.db")
 
-engine = create_engine(DATABASE_URL=postgres:DZqWAvUhwrHBWabrlJgNBrYbDVmufWnu@yamabiko.proxy.rlwy.net:36404/railway)
+engine = create_engine(DATABASE_URL=postgres://DZqWAvUhwrHBWabrlJgNBrYbDVmufWnu@yamabiko.proxy.rlwy.net:36404/railway)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
