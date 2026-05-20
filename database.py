315c1fc318734ecbb412dc86cf9f0c172bf455fc
ismagulovmurat861-collab <ismagulovmurat861@gmail.com>
2026-05-20 18:00:03 +0600

    Fix duplicate engine creation in database.py

--- database.py
+++ database.py
@@ -6,5 +6,7 @@ DATABASE_URL = os.getenv("DATABASE_URL", "")
 DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://")
 
 engine = create_engine(DATABASE_URL)
+
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
